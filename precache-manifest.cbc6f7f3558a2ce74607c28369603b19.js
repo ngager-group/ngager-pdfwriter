bc759d5850651e84e70006e830eff198ea77681e
Long Nguyen <nclong87@gmail.com>
2020-02-26 14:38:21 +0800

    Updates

---
+++ precache-manifest.cbc6f7f3558a2ce74607c28369603b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d493384aeb4a2216cbdaddf479d0ab1e",
+    "url": "/ngager-datepicker/index.html"
+  },
+  {
+    "revision": "5965d060b021f9b9a4d0",
+    "url": "/ngager-datepicker/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "d5a1cbc22c61fa83c6ac",
+    "url": "/ngager-datepicker/static/js/2.0a2d3816.chunk.js"
+  },
+  {
+    "revision": "c9fbe2e5cb868793d6cbe5987f97ebea",
+    "url": "/ngager-datepicker/static/js/2.0a2d3816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5965d060b021f9b9a4d0",
+    "url": "/ngager-datepicker/static/js/main.66424be3.chunk.js"
+  },
+  {
+    "revision": "c9bead6244f5339adba8",
+    "url": "/ngager-datepicker/static/js/runtime-main.8359a3d8.js"
+  }
+]);
